0706f00f125914b5a5652977d011adac593f4acd
Gautam Subramanian <dev2025.gs@gmail.com>
2026-01-07 11:35:03 -0500

    fix: remove duplicate BrowserRouter, keep only HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Sandbox from "./pages/Sandbox";
 import ManeuverPlanner from "./pages/ManeuverPlanner";
@@ -18,18 +18,16 @@ const App = () => (
         <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/sandbox" element={<Sandbox />} />
-                    <Route path="/maneuver" element={<ManeuverPlanner />} />
-                    <Route path="/academy" element={<Academy />} />
-                    <Route path="/settings" element={<Settings />} />
-                    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sandbox" element={<Sandbox />} />
+                <Route path="/maneuver" element={<ManeuverPlanner />} />
+                <Route path="/academy" element={<Academy />} />
+                <Route path="/settings" element={<Settings />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </TooltipProvider>
     </QueryClientProvider>
 );
